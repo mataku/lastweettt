a74206884a1db4843fda5adb89fe7c264e5a3f4c
Homma Takuma <sfprhythnn@gmail.com>
2016-08-30 21:51:35 +0900

    いらない所を削除した

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 ## Installation
-#### 事前設定
+### 事前設定
 1. Rustをインストール
-2. twitter_key_templates.yamlに自分のkey, tokenを書く
-3. `$ mv twitter_key_templates.yaml twitter_key.yaml`
+2. `$ cp twitter_key_templates.yaml twitter_key.yaml`
+3. twitter_key.yaml に取得したkey, tokenを書く
 
-#### Install to `~/.cargo/bin`
+### Install to `~/.cargo/bin`
 ```bash
 $ cargo install
 ```
 
-#### とりあえず動かしたい場合
+### とりあえず動かしたい場合
 ```bash
 $ cargo run
 ```
 
-#### opensslに関連するコンパイルエラー
+### opensslに関連するコンパイルエラー
 - HomebrewのOpenSSLの場合
 ```bash
 $ export OPENSSL_INCLUDE_DIR=$(brew --prefix openssl)/include
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ extern crate oauthcli;
 extern crate serde_json;
 extern crate serde_yaml;
 
+//use std::env::args;
 use std::fs::File;
 use std::io::Read;
+use std::collections::HashMap;
 use hyper::Url;
 use hyper::client::Client;
 use hyper::header::Authorization;
 use oauthcli::{SignatureMethod, authorization_header, nonce, timestamp};
 use serde_json::Value;
-use std::collections::HashMap;
 
 fn load_config() -> HashMap<String, String> {
     let mut yaml_file = File::open("twitter_key.yaml").unwrap();
@@ -20,40 +21,29 @@ fn load_config() -> HashMap<String, String> {
     return config;
 }
 
+
+
 fn main() {
 
-	let config: HashMap<String, String> = load_config();
+    let config: HashMap<String, String> = load_config();
     
-    let base = Url::parse("https://api.twitter.com/1.1/statuses/user_timeline.json").unwrap();
+    let base_url = Url::parse("https://api.twitter.com/1.1/statuses/user_timeline.json").unwrap();
     let params = vec![
         ("screen_name".to_owned(), "donuuuuts".to_owned()),
         ("count".to_owned(), "1".to_owned()),
-        //("trum_user".to_owned(), "0".to_owned()),
-        ("contributor_details".to_owned(), "false".to_owned()),
         ("include_rts".to_owned(), "false".to_owned()),
         ("trim_user".to_owned(), "false".to_owned()),
     ];
-    let mut url = base.clone();
-	//url.set_query_from_pairs(params.iter().map(|&(ref k, ref v)| {
-    //    (&k[..], &v[..])
-    //}));
-    url.query_pairs_mut()
-        .append_pair("screen_name", "donuuuuts");
-        
-    url.query_pairs_mut()
-        .append_pair("count", "1");
-
-    url.query_pairs_mut()
-        .append_pair("trim_user", "false");
+    let mut url = base_url.clone();
     
     url.query_pairs_mut()
-        .append_pair("include_rts", "false");
-
-    url.query_pairs_mut()
-        .append_pair("contributor_details", "false");
-
+        .append_pair("screen_name", "donuuuuts")
+        .append_pair("count", "1")
+        .append_pair("include_rts", "false")
+        .append_pair("trim_user", "false");
+    
 	let auth = authorization_header(
-        "GET", base, None,
+        "GET", base_url, None,
         config["consumer_key"].as_str(), config["consumer_secret"].as_str(),
         Some(config["access_token"].as_str()), Some(config["access_token_secret"].as_str()),
         SignatureMethod::HmacSha1,
